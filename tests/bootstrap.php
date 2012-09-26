8bb1f553ae7840c15bd30ae926f00e83a0e6873e
Chris Cornutt <enygma@phpdeveloper.org>
2012-09-25 21:06:30 -0500

    swapping out composer autoloader with one of its own

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 
-// include the composer autoloader
-require_once '../../autoload.php';
+// make a little autoloader
+spl_autoload_register(function($className){
+    $path = __DIR__.'/../../'.str_replace('\\','/',$className).'.php';
+    if (is_file(realpath($path))) {
+        require_once realpath($path);
+    }
+});
+
 
 ?>
