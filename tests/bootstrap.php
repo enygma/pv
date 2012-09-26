5bda93cd31d97e3d27700afd1cf263cfdb67d393
Chris Cornutt <enygma@phpdeveloper.org>
2012-09-25 21:17:35 -0500

    updating to output the path for the include in autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,8 @@
 // make a little autoloader
 spl_autoload_register(function($className){
     $path = __DIR__.'/../../'.str_replace('\\','/',$className).'.php';
+    echo 'PATH: '.$path."\n\n";
+    
     if (is_file(realpath($path))) {
         require_once realpath($path);
     }
