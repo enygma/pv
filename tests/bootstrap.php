2facaf1d822407d04a95198239fb17b5a4767650
Chris Cornutt <enygma@phpdeveloper.org>
2012-09-25 21:23:31 -0500

    updating to use relative pathing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,7 @@
 
 // make a little autoloader
 spl_autoload_register(function($className){
-    $path = __DIR__.'/../../'.str_replace('\\','/',$className).'.php';
-    echo 'PATH: '.$path."\n\n";
-    
+    $path = __DIR__.'/..'.str_replace(array('Pv\\','\\'),'/',$className).'.php';
     if (is_file(realpath($path))) {
         require_once realpath($path);
     }
