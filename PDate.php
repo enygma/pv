6c20250131b4b65e9aaf67fe8d6a725db984f1ed
Chris Cornutt <enygma@phpdeveloper.org>
2012-11-02 20:11:14 -0400

    adding toString for date object

--- PDate.php
+++ PDate.php
@@ -7,12 +7,21 @@ class PDate extends Variable
     public function __construct($value,$type=null)
     {
         try {
-            $dt = new \DateTime($value);    
+            $value = new \DateTime($value);    
         } catch(\Exception $e) {
             throw new ValidationException('Could not create date object, bad input');
         }
         
         parent::__construct($value,$type);
+    }
+
+    /**
+     * Convert the DateTime object to a string RFC-2822 string
+     * @return string
+     */
+    public function toString()
+    {
+        return ('r', $this->value);
     }
 }
 
